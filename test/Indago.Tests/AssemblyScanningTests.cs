97cffc7b091114c16af74c35c5c28e8f632300fe
David Driscoll <david.driscoll@gmail.com>
2026-06-25 20:02:57 -0400

    Update project files to include System.Text.Json path properties and … (#111)

--- test/Indago.Tests/AssemblyScanningTests.cs
+++ test/Indago.Tests/AssemblyScanningTests.cs
@@ -24,6 +24,7 @@ public partial class AssemblyScanningTests : GeneratorTest
     }
 
     [Test]
+    [DependsOn(nameof(Should_Generate_All_The_Things), ProceedOnFailure = true)]
     [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
     public async Task Should_Generate_All_The_Things_From_Another_Assembly(TestSource item, CancellationToken cancellationToken)
     {
@@ -54,37 +55,8 @@ public partial class AssemblyScanningTests : GeneratorTest
     }
 
     [Test]
+    [DependsOn(nameof(Should_Generate_All_The_Things), ProceedOnFailure = true)]
     [DependsOn(nameof(Should_Generate_All_The_Things_From_Another_Assembly), ProceedOnFailure = true)]
-    [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
-    public async Task Should_Generate_All_The_Things_From_Another_Assembly_Using_Cache(TestSource item, CancellationToken cancellationToken)
-    {
-        using var assemblyLoadContext = new CollectibleTestAssemblyLoadContext();
-        var other = await Builder
-                         .WithProjectName("OtherProject")
-                         .AddSources(item.Source)
-                         .AddReferences(typeof(IService))
-                         .PopulateCache(item.GetTempDirectory("other"))
-                         .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft.Extensions.DependencyInjection")
-                         .Build()
-                         .GenerateAsync(cancellationToken);
-
-        other.FinalDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Error).ShouldBeEmpty();
-        other.EnsureDiagnosticSeverity(DiagnosticSeverity.Error);
-
-        var result = await Builder
-                          .AddCompilationReferences(other)
-                          .AddReferences(typeof(IService))
-                          .PopulateCache(item.GetTempDirectory("test"))
-                          .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft.Extensions.DependencyInjection")
-                          .Build()
-                          .GenerateAsync(cancellationToken);
-
-        await Verify(result.AddCacheFiles())
-             .AddScrubber(z => z.Replace(item.GetTempDirectory(), "{TempPath}"))
-             .UseParameters(item.FileSafeName);
-    }
-
-    [Test]
     [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
     public async Task Should_Generate_All_The_Things_From_Self_And_Another_Assembly(TestSource item, CancellationToken cancellationToken)
     {
@@ -116,7 +88,63 @@ public partial class AssemblyScanningTests : GeneratorTest
     }
 
     [Test]
+    [DependsOn(nameof(Should_Generate_All_The_Things), ProceedOnFailure = true)]
+    [DependsOn(nameof(Should_Generate_All_The_Things_From_Another_Assembly), ProceedOnFailure = true)]
     [DependsOn(nameof(Should_Generate_All_The_Things_From_Self_And_Another_Assembly), ProceedOnFailure = true)]
+    [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
+    public async Task Should_Generate_All_The_Things_From_Another_Assembly_Using_Cache(TestSource item, CancellationToken cancellationToken)
+    {
+        using var assemblyLoadContext = new CollectibleTestAssemblyLoadContext();
+        var other = await Builder
+                         .WithProjectName("OtherProject")
+                         .AddSources(item.Source)
+                         .AddReferences(typeof(IService))
+                         .PopulateCache(item.GetTempDirectory("other"))
+                         .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft.Extensions.DependencyInjection")
+                         .Build()
+                         .GenerateAsync(cancellationToken);
+
+        other.FinalDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Error).ShouldBeEmpty();
+        other.EnsureDiagnosticSeverity(DiagnosticSeverity.Error);
+
+        var result = await Builder
+                          .AddCompilationReferences(other)
+                          .AddReferences(typeof(IService))
+                          .PopulateCache(item.GetTempDirectory("test"))
+                          .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft.Extensions.DependencyInjection")
+                          .Build()
+                          .GenerateAsync(cancellationToken);
+
+        await Verify(result.AddCacheFiles())
+             .AddScrubber(z => z.Replace(item.GetTempDirectory(), "{TempPath}"))
+             .UseParameters(item.FileSafeName);
+    }
+
+    [Test]
+    [DependsOn(nameof(Should_Generate_All_The_Things), ProceedOnFailure = true)]
+    [DependsOn(nameof(Should_Generate_All_The_Things_From_Another_Assembly), ProceedOnFailure = true)]
+    [DependsOn(nameof(Should_Generate_All_The_Things_From_Self_And_Another_Assembly), ProceedOnFailure = true)]
+    [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
+    public async Task Should_Generate_All_The_Things_Using_Cache(TestSource item, CancellationToken cancellationToken)
+    {
+        var result = await Builder
+                          .AddSources(item.Source)
+                          .AddReferences(typeof(IService))
+                          .PopulateCache(item.GetTempDirectory())
+                          .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft")
+                          .Build()
+                          .GenerateAsync(cancellationToken);
+
+        await Verify(result.AddCacheFiles())
+             .AddScrubber(z => z.Replace(item.GetTempDirectory(), "{TempPath}"))
+             .UseParameters(item.FileSafeName);
+    }
+
+    [Test]
+    [DependsOn(nameof(Should_Generate_All_The_Things), ProceedOnFailure = true)]
+    [DependsOn(nameof(Should_Generate_All_The_Things_From_Another_Assembly), ProceedOnFailure = true)]
+    [DependsOn(nameof(Should_Generate_All_The_Things_From_Self_And_Another_Assembly), ProceedOnFailure = true)]
+    [DependsOn(nameof(Should_Generate_All_The_Things_From_Another_Assembly_Using_Cache), ProceedOnFailure = true)]
     [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
     public async Task Should_Generate_All_The_Things_From_Self_And_Another_Assembly_Using_Cache(TestSource item, CancellationToken cancellationToken)
     {
@@ -139,24 +167,6 @@ public partial class AssemblyScanningTests : GeneratorTest
                           .AddReferences(typeof(IService))
                           .PopulateCache(item.GetTempDirectory("test"))
                           .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft.Extensions.DependencyInjection.Abstractions")
-                          .Build()
-                          .GenerateAsync(cancellationToken);
-
-        await Verify(result.AddCacheFiles())
-             .AddScrubber(z => z.Replace(item.GetTempDirectory(), "{TempPath}"))
-             .UseParameters(item.FileSafeName);
-    }
-
-    [Test]
-    [DependsOn(nameof(Should_Generate_All_The_Things), ProceedOnFailure = true)]
-    [MethodDataSource(typeof(AssemblyScanningTestData), nameof(AssemblyScanningTestData.GetTestData))]
-    public async Task Should_Generate_All_The_Things_Using_Cache(TestSource item, CancellationToken cancellationToken)
-    {
-        var result = await Builder
-                          .AddSources(item.Source)
-                          .AddReferences(typeof(IService))
-                          .PopulateCache(item.GetTempDirectory())
-                          .AddGlobalOption("build_property.ExcludeAssemblyFromCTP", "Microsoft")
                           .Build()
                           .GenerateAsync(cancellationToken);
 
