b3c9956dba3101b4e79581176b99315605844dcf
David Driscoll <david.driscoll@gmail.com>
2026-07-19 17:10:14 -0400

    updating snapshots

--- test/Specular.Tests/snapshots/StaticScanningTests.Should_Filter_Namespaces_filter=NotIn_namespaceFilterValue=TestProject.A.C.ServiceC_usingClass=True_usingTypeof=True#SpecularProvider.g.verified.cs
+++ test/Specular.Tests/snapshots/StaticScanningTests.Should_Filter_Namespaces_filter=NotIn_namespaceFilterValue=TestProject.A.C.ServiceC_usingClass=True_usingTypeof=True#SpecularProvider.g.verified.cs
@@ -1,4 +1,4 @@
-//HintName: Specular.Analyzers/Specular.Analyzers.SpecularProviderGenerator/SpecularProvider.g.cs
+﻿//HintName: Specular.Analyzers/Specular.Analyzers.SpecularProviderGenerator/SpecularProvider.g.cs
 #nullable enable
 #pragma warning disable CA1002, CA1034, CA1822, CS0105, CS1573, CA5351, CS8618, CS8669, IL2026, IL2072
 using System;
